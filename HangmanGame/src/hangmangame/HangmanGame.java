e8a694ea7102bfe19fe5108fdd5df565b99a971e
Marcus Wood <Marcus Wood@MarcusComputer>
2013-04-30 16:57:04 -0500

    uploading

---
+++ HangmanGame/src/hangmangame/HangmanGame.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hangmangame;
+
+/**
+ *
+ * @author Marcus Wood
+ */
+public class HangmanGame {
+
+    /**
+     * @param args the command line arguments
+     */
+
+      public static void main(String[] args) {
+        Hangman game = new Hangman();
+        game.playGame();
+        
+        
+        //hi
+    }
+    
+}
